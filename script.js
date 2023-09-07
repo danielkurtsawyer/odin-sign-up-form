c7892b3a4b83abd4787aec5ea760a1b7722fb9c8
danielkurtsawyer <19danielsawyer@gmail.com>
2023-09-07 17:40:15 -0400

    Implement the rest of the site.
    I finished this project by implementing the rest of the functionality as well as finishing the CSS styling.
    The site has a darker theme to suggest the vastness and emptiness of space.
    I just a script to check the password fields to see if they match. If they do, then the error class is toggled and the error message is hidden.

---
+++ script.js
@@ -0,0 +1,17 @@
+const passwordInputs = document.querySelectorAll(".error");
+const passwordMessage = document.querySelector(".error-message");
+
+passwordInputs.forEach(input => input.addEventListener('blur', validatePasswordMatches));
+
+const passwordElements = {...passwordInputs};
+
+function validatePasswordMatches(e){
+    if(passwordElements[0].value === passwordElements[1].value){
+        passwordInputs.forEach(input => input.classList.remove('error'));
+        passwordMessage.style.display = "none";
+    }
+    else{
+        passwordInputs.forEach(input => input.classList.add('error'));
+        passwordMessage.style.display = "inline";
+    }
+}
